c11343e018e17d39e1eeaec3865321bb9ca68f00
Captain Wiseman <pilgriman@gmail.com>
2020-03-03 22:23:05 +0200

    cleaned and checking now capabilities

--- serv/events/eventsModule.js
+++
@@ -1,7 +0,0 @@
-import { readEventsJSONConfig } from "../data-modules/configsModule";
-const log = require("../../common/logger");
-
-export const rollForEvents = async () => {
-  const cfg = await readEventsJSONConfig();
-  log(`Events, ${JSON.stringify(cfg)}`);
-};
