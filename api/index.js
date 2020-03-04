8407dad069aa4274a03033757c996a0a2ad73adb
liorwi <liorwi@wix.com>
2020-03-04 10:37:55 +0200

    cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import { getWorldData } from "../serv/http-functions";
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   const world = await getWorldData();
+
   res.send(world);
 };
