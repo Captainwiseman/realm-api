fca12f5cbfa718478a2e30972888709865e32f75
liorwi <liorwi@wix.com>
2020-03-04 10:29:44 +0200

    more test

--- serv/http-functions.js
+++ serv/http-functions.js
@@ -1,9 +1,8 @@
 import dbModule from "./data-modules/dbModule";
 import _ from "lodash";
 
-export async function getWorldData(req, res) {
+export async function getWorldData() {
   console.log("Requesting World...");
   const world = await dbModule.readAllHistoryDataFromDB();
-
-  return res.json(world);
+  return world;
 }
