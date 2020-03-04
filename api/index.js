ebb09fc310004560496e9ccdc7328b77b7225b09
Captain Wiseman <pilgriman@gmail.com>
2020-03-04 21:37:05 +0200

    hopefully managed cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import { getWorldData } from "../serv/http-functions";
+import Cors from "micro-cors";
 
-module.exports = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+const cors = Cors({
+  origin: "http://localhost:3000"
+});
+
+const mainEndPointHandler = async (req, res) => {
   const world = await getWorldData();
 
   res.send(world);
 };
+
+export default cors(mainEndPointHandler);
